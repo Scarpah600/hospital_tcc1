bbec2c3a89a1533613d5fb57c87cb7532e5cf6f3
Scarpah600 <gustavo.scarpin.2020@gmail.com>
2020-02-09 16:39:16 -0300

    09.02.2020-Atualizando/SQL

--- src/main/java/br/com/hospital/mmgservices/HospitalApplication.java
+++ src/main/java/br/com/hospital/mmgservices/HospitalApplication.java
@@ -1,13 +1,29 @@
 package br.com.hospital.mmgservices;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HospitalApplication {
+import br.com.hospital.mmgservices.domain.Categoria;
+import br.com.hospital.mmgservices.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class HospitalApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		//Operacao de instanciação automatico
+	
 	}
 
 }
